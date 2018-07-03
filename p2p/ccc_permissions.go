6f504c28b0a5c6d12dc8a0d075467b5d932df276
王s <ssss@wangsdeMacBook-Pro.local>
2018-07-03 16:49:00 +0800

    add  CCC permissions New node web access logic

---
+++ p2p/ccc_permissions.go
@@ -0,0 +1,213 @@
+
+
+package p2p
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/p2p/discover"
+	"bufio"
+	"io"
+	"strings"
+	"net/http"
+	"os/exec"
+)
+
+const (
+	NODE_NAME_LENGTH    = 32
+	PERMISSIONED_CONFIG = "permissioned-nodes.conf"
+)
+
+const middle = "========="
+
+type PConfig struct {
+	Mymap  map[string]string
+	strcet string
+}
+func getCurrentPath() string {
+	s, err := exec.LookPath(os.Args[0])
+	checkErr(err)
+	i := strings.LastIndex(s, "\\")
+	path := string(s[0 : i+1])
+	return path
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (c *PConfig) InitConfig(path string) {
+	c.Mymap = make(map[string]string)
+
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		b, _, err := r.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		s := strings.TrimSpace(string(b))
+		//fmt.Println(s)
+		if strings.Index(s, "#") == 0 {
+			continue
+		}
+
+		n1 := strings.Index(s, "[")
+		n2 := strings.LastIndex(s, "]")
+		if n1 > -1 && n2 > -1 && n2 > n1+1 {
+			c.strcet = strings.TrimSpace(s[n1+1 : n2])
+			continue
+		}
+
+		if len(c.strcet) == 0 {
+			continue
+		}
+		index := strings.Index(s, "=")
+		if index < 0 {
+			continue
+		}
+
+		frist := strings.TrimSpace(s[:index])
+		if len(frist) == 0 {
+			continue
+		}
+		second := strings.TrimSpace(s[index+1:])
+
+		pos := strings.Index(second, "\t#")
+		if pos > -1 {
+			second = second[0:pos]
+		}
+
+		pos = strings.Index(second, " #")
+		if pos > -1 {
+			second = second[0:pos]
+		}
+
+		pos = strings.Index(second, "\t//")
+		if pos > -1 {
+			second = second[0:pos]
+		}
+
+		pos = strings.Index(second, " //")
+		if pos > -1 {
+			second = second[0:pos]
+		}
+
+		if len(second) == 0 {
+			continue
+		}
+
+		key := c.strcet + middle + frist
+		c.Mymap[key] = strings.TrimSpace(second)
+	}
+}
+
+func (c PConfig) Read(node, key string) string {
+	key = node + middle + key
+	v, found := c.Mymap[key]
+	if !found {
+		return ""
+	}
+	return v
+}
+
+
+// check if a given node is permissioned to connect to the change
+func isNodePermissioned(nodename string, currentNode string, datadir string, direction string) bool {
+
+	var permissionedList []string
+	nodes := parsePermissionedNodes(datadir)
+	for _, v := range nodes {
+		permissionedList = append(permissionedList, v.ID.String())
+	}
+
+	log.Debug("isNodePermissioned", "permissionedList", permissionedList)
+	for _, v := range permissionedList {
+		if v == nodename {
+			log.Debug("isNodePermissioned", "connection", direction, "nodename", nodename[:NODE_NAME_LENGTH], "ALLOWED-BY", currentNode[:NODE_NAME_LENGTH])
+			return true
+		}
+		log.Debug("isNodePermissioned", "connection", direction, "nodename", nodename[:NODE_NAME_LENGTH], "DENIED-BY", currentNode[:NODE_NAME_LENGTH])
+	}
+	log.Debug("isNodePermissioned", "connection", direction, "nodename", nodename[:NODE_NAME_LENGTH], "DENIED-BY", currentNode[:NODE_NAME_LENGTH])
+	return false
+}
+
+//this is a shameless copy from the config.go. It is a duplication of the code
+//for the timebeing to allow reload of the permissioned nodes while the server is running
+
+func parsePermissionedNodes(DataDir string) []*discover.Node {
+
+	log.Debug("parsePermissionedNodes", "DataDir", DataDir, "file", PERMISSIONED_CONFIG)
+
+	path := filepath.Join(DataDir, PERMISSIONED_CONFIG)
+	//paths := getCurrentPath()
+
+	if _, err := os.Stat(path); err != nil {
+		log.Error("Read Error for permissioned-nodes.conf file. "  , "This is because 'permissioned' flag is specified but no permissioned-nodes.conf file is present.", "err", err)
+		return nil
+	}
+
+	PConfig := new(PConfig)
+	PConfig.InitConfig(path)
+	url := PConfig.Read("default", "url")
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Error("CCC Network is unavailable", "err", err)
+		return nil
+
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error("CCC Failed to load nodes", "err", err)
+		return nil
+
+
+	}
+
+
+	// Load the nodes from the config file
+	//blob, err := ioutil.ReadFile(path)
+	//if err != nil {
+	//	log.Error("parsePermissionedNodes: Failed to access nodes", "err", err)
+	//	return nil
+	//}
+
+	nodelist := []string{}
+	if err := json.Unmarshal(body, &nodelist); err != nil {
+		log.Error("parsePermissionedNodes: Failed to load nodes", "err", err)
+		return nil
+	}
+	// Interpret the list as a discovery node array
+	var nodes []*discover.Node
+	for _, url := range nodelist {
+		if url == "" {
+			log.Error("parsePermissionedNodes: Node URL blank")
+			continue
+		}
+		node, err := discover.ParseNode(url)
+		if err != nil {
+			log.Error("parsePermissionedNodes: Node URL",  "url", url, "err", err)
+			continue
+		}
+		//log.Error("json node:   "+node.String())
+		nodes = append(nodes, node)
+	}
+	return nodes
+}
