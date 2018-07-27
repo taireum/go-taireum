b22ca7179240a694b57ebc946b6aa8a161a11dca
Zhihui Li <zhihui.li@intel.com>
2018-07-27 11:39:00 +0800

    pull contract address and poa addressess from ccc

--- consensus/tai/authority.go
+++ consensus/tai/authority.go
@@ -1,33 +1,51 @@
 package tai
 
-import(
+import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/params"
 	"bytes"
+	"github.com/ethereum/go-ethereum/tai"
+)
+
+var (
+	authoritiesUrl = "http://localhost:8421/authorities"
+	contractUrl    = "http://localhost:8421/contrac"
 )
 
 type Authority struct {
-	Signers map[common.Address]struct{}
-	Url string
 }
 
-func newAuthority(config *params.TaiConfig) *Authority{
+func newAuthority(config *params.TaiConfig) *Authority {
+
+	return &Authority{}
+}
+
+//get authority address from ccc
+func (a *Authority) Signers() map[common.Address]struct{} {
 	s := make(map[common.Address]struct{})
-
-	////TODO signer list should be pull from CCC
-	s[common.HexToAddress("0x06a0b23e787ec10bd70b993f52c5f8550f5f5c04")]=struct{}{}
-
-	authority := &Authority{
-		s,
-		"",
+	authorityAddresses := tai.ListFromUrl(authoritiesUrl)
+	if (authorityAddresses != nil) {
+		for _, authorityAddress := range authorityAddresses {
+			s[common.HexToAddress(authorityAddress)] = struct{}{}
+		}
 	}
-	return authority
+	return s
+}
+
+// get vote contract address from ccc
+// the function is called onle once for whole blockchain only in founder node
+func (a *Authority) contractAddress() common.Address {
+	contractAddresses := tai.ListFromUrl(contractUrl)
+	if (contractAddresses != nil && len(contractAddresses) > 1) {
+		return common.HexToAddress(contractAddresses[0])
+	}
+	return common.Address{}
 }
 
 // signers retrieves the list of authorized signers in ascending order.
 func (a *Authority) signers() []common.Address {
-	signers := make([]common.Address, 0, len(a.Signers))
-	for signer := range a.Signers {
+	signers := make([]common.Address, 0, len(a.Signers()))
+	for signer := range a.Signers() {
 		signers = append(signers, signer)
 	}
 	for i := 0; i < len(signers); i++ {
